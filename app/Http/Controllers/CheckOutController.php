51867f46f10af955a4b1678330da030d027e0b6b
Bikal <bkalkonda12@gmail.com>
2022-10-31 21:13:45 +0545

    3 field added

--- app/Http/Controllers/CheckOutController.php
+++ app/Http/Controllers/CheckOutController.php
@@ -15,7 +15,9 @@ class CheckOutController extends Controller
         $checkoutitem->last_name = $req->input('last_name');
         $checkoutitem->address = $req->input('address');
         $checkoutitem->phone_no = $req->input('phone_no');
-        $checkoutitem->email = $req->input('email');
+        $checkoutitem->email = $req->input('price');
+        $checkoutitem->email = $req->input('productname');
+        $checkoutitem->email = $req->input('quantity');
         $checkoutitem->save();
         return $checkoutitem;
     }
